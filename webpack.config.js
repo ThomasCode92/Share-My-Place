2b1bf83ffae6383b9f922ef618bc903e2cd655a8
ThomasCode92 <thomascode92@hotmail.com>
2022-11-23 13:05:07 +0100

    Setting Up the Development Environment

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+/*eslint-env node*/
+
+const path = require('path');
+
+const CleanPlugin = require('clean-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const ESLintWebpackPlugin = require('eslint-webpack-plugin');
+
+const { CleanWebpackPlugin } = CleanPlugin;
+
+module.exports = {
+  mode: 'development',
+  entry: './src/scripts/app.js',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'eval-cheap-module-source-map',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname),
+    },
+    open: true,
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { useBuiltIns: 'usage', corejs: '3.26' }],
+            ],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HTMLWebpackPlugin({
+      title: 'Project Board',
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+    new ESLintWebpackPlugin(),
+  ],
+};
