5d65e3b5132d7c1da79ef2ec3493a4c7d13779ab
ThomasCode92 <thomascode92@hotmail.com>
2022-11-30 12:45:30 +0100

    Rendering the "Shared Place" Screen

--- src/scripts/app/my-place.js
+++ src/scripts/app/my-place.js
@@ -1,2 +1,23 @@
+import Map from '../UI/map';
+
 import '../../styles/app.css';
 import '../../styles/my-place.css';
+
+class LoadedPlace {
+  constructor(coordinates, address) {
+    new Map(coordinates);
+
+    const headerTitleElement = document.querySelector('header h1');
+    headerTitleElement.textContent = address;
+  }
+}
+
+const url = new URL(location.href);
+const queryParams = url.searchParams;
+const coordinates = {
+  lat: parseFloat(queryParams.get('lat')),
+  lng: parseFloat(queryParams.get('lng')),
+};
+const address = queryParams.get('address');
+
+new LoadedPlace(coordinates, address);
